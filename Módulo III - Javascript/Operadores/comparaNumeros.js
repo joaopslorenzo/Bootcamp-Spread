9fbccbc68b8a4816e435861a4952491b369410ad
João Pedro <joaopslorenzo@gmail.com>
2022-05-11 11:09:39 -0300

    Altera comparaNumeros.js

--- Módulo III - Javascript/Operadores/comparaNumeros.js
+++ Módulo III - Javascript/Operadores/comparaNumeros.js
@@ -1,17 +1,31 @@
-var num1 = 10;
-var num2 = 20;
-
-if (num1 === num2){
-    console.log(`O número ${num1} é igual ao ${num2} `)
-}else{
-    var soma = num1 + num2;
-    if(soma > 10){
-        if(soma > 20){
-            console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20.`);
+function verificaNumeros (num1, num2){
+    if(!num1 || !num2) {
+        return console.log('Defina dois números');
+    }
+    if (num1 === num2){
+        var soma = num1 + num2;
+        if(soma > 10){
+            if(soma > 20){
+                console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20.`);
+            }else{
+                console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20.`);
+            }
         }else{
-            console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20.`);
+            console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20.`);
         }
     }else{
-        console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20.`);
+        var soma = num1 + num2;
+        if(soma > 10){
+            if(soma > 20){
+                console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20.`);
+            }else{
+                console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20.`);
+            }
+        }else{
+            console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20.`);
+        }
     }
 }
+
+verificaNumeros(10, 10);
+
